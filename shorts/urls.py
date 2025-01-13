9bebc9f32e92301549eabab2ad1f89d71eabb1c6
daha-lee <dlekgk03@naver.com>
2025-01-14 02:31:37 +0900

    feat:사이트 방문 수 증가

--- shorts/urls.py
+++ shorts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ShortsAPIView
+from .views import ShortsAPIView, ShortVisitAPIView
 
 urlpatterns = [
     path('', ShortsAPIView.as_view()),
+    path('<int:book_id>/visit', ShortVisitAPIView.as_view(), name='short-visit-api'),
 ]
