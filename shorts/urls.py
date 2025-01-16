2bfc05b7c8e3dfa9583adcc8b5cfe4bac7ded1ce
BOOK-DKVO3F11TA\ygm27 <yhm277@nave.com>
2025-01-16 16:49:16 +0900

    feat: 숏츠 설명창 API

--- shorts/urls.py
+++ shorts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ShortsAPIView, ShortVisitAPIView, ShortShareAPIView
+from .views import ShortsAPIView, ShortVisitAPIView, ShortShareAPIView, ShortDetailAPIView
 
 urlpatterns = [
     path('', ShortsAPIView.as_view()),
     path('<int:book_id>/visit', ShortVisitAPIView.as_view(), name='short-visit-api'),
     path('<int:book_id>/share', ShortShareAPIView.as_view(), name='short-share-api'),
+    path('<int:book_id>/detail', ShortDetailAPIView.as_view(), name ='short-detail-api'),
 ]
