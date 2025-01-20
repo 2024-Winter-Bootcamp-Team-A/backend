7dffb0d188ec4b1c5db50805c486898d6a141ba8
BOOK-DKVO3F11TA\ygm27 <yhm277@nave.com>
2025-01-20 22:28:17 +0900

    feat : Most Popular Shorts API

--- stats/urls.py
+++ stats/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MostViewedAPIView, MostCommentedAPIView, MostWishedAPIView
+from .views import MostViewedAPIView, MostCommentedAPIView, MostWishedAPIView, MostPopularAPIView
+
 
 urlpatterns = [
     path('most-viewed', MostViewedAPIView.as_view(), name='most-viewed'),
     path('most-commented', MostCommentedAPIView.as_view(), name='most-commented'),
     path('most-wished', MostWishedAPIView.as_view(), name='most-wished'),
+    path('most-popular', MostPopularAPIView.as_view(), name='most-popular'),
 ]
